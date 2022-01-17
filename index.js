9a067a80d203a7c3b7d65f4ecce2485f1f4833ff
Gustavo Lima <gustavolimaeduardo@gmail.com>
2022-01-17 12:39:41 -0300

    base inicial

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+
+
+//Controllers
+const atendimentoController = require("./controllers/atendimentoController");
+
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.use(express.json({limit: '150mb'}));
+app.use(express.urlencoded({ extended: false }));
+
+//rotas
+
+//pagina Home
+app.get('/', (req, res) => {
+	res.redirect("/");	
+});
+
+
+//Routers
+
+app.use('/',atendimentoController);
+
+
+//Iniciando o servidor
+app.listen(4000,(err) =>{
+	if(err){
+		console.log(err)
+	}else{
+		console.log("Front rodando na porta 4000.")
+	}
+});
